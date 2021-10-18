42e8ddcc9f65d6d3955db5cb14094d8329aa8755
Sandaru Ashen <sandaruashen02@gmail.com>
2021-10-18 14:42:22 +0000

    v2.5 enhancements

---
+++ bomber.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+# By Sandaru Ashen: https://github.com/Sl-Sanda-Ru,https://t.me/Sl_Sanda_Ru
+
+import os
+import sys
+
+PYTHON_VERSION = '.'.join(str(i) for i in sys.version_info[:2])
+
+if PYTHON_VERSION not in ('3.8', '3.9', '3.10'):
+    print('\x1b[1m\x1b[91m [!] Your Python Version Is Not Supported'.center(os.get_terminal_size().columns))
+    exit('\x1b[1m\x1b[91m [!] Please Update Your Python'.center(os.get_terminal_size().columns))
+if os.name == 'nt':
+    os.system('python .{}.py'.format(PYTHON_VERSION))
+else:
+    os.system('python{} .{}.py'.format(PYTHON_VERSION, PYTHON_VERSION))
